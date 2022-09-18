f068c85f1316b77516ce9646a145e9219fa3b1db
ahenrique <alisson90001@gmail.com>
2022-09-18 12:41:00 -0300

    Update lendo_pastas_arquivos.py

--- lendo_pastas_arquivos.py
+++ lendo_pastas_arquivos.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-def escanear_pasta(pasta_inicial, pasta='', nivel=0):
+def escanear_pasta_os(pasta_inicial, pasta='', nivel=0):
 
     caminho = os.path.join(pasta_inicial, pasta)
     if not os.path.isdir(caminho):
@@ -10,10 +11,21 @@ def escanear_pasta(pasta_inicial, pasta='', nivel=0):
 
     for arquivo in arquivos:
         print('\t'*nivel, '>>' if not os.path.isfile(os.path.join(caminho,arquivo)) else '>', arquivo)
-        escanear_pasta(caminho, arquivo, nivel+1)
+        escanear_pasta_os(caminho, arquivo, nivel+1)
+
+
+def escanear_pasta_path(pasta_inicial, pasta='', nivel=0):    
+    caminho = Path().joinpath(pasta_inicial, pasta)
+    if not caminho.is_dir():
+        return
+    arquivos = caminho.iterdir()
+    for arquivo in arquivos:
+        print('\t' * nivel, '>>' if not arquivo.is_file() else '>', arquivo.name)
+        escanear_pasta_path(caminho, arquivo.name, nivel + 1)
+        
 
         
 path = r''
 
-escanear_pasta(path)
+escanear_pasta_path(path)
 
